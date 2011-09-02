8a1d2c6fd858e7800bf9e8373daca501ad99b890
dead-horse <dead_horse@qq.com>
2011-09-02 10:14:11 +0800

    ad

--- controllers/manager.js
+++ controllers/manager.js
@@ -401,14 +401,15 @@ exports.applog = function(req, res){
 	email:req.session.email,domain:domain});
 };
 exports.getStdOutput = function(req, res){
-	console.log(req);
 	var domain = req.params.id||'',
 		action = req.body.action;
 		console.log("output");
 		console.log(domain + action);
 	getLog(action, domain, 1000, function(data){
 		console.log("resAjax "+action);
+		try{
 		return resAjax(res, {output:data});
+		}catch(e){console.log(e.message)};
 	});
 }
 
