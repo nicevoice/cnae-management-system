fff214371ea8b7d1db42a876dc9ad00627419c7a
dead-horse <dead_horse@qq.com>
2011-09-05 13:40:48 +0800

    change socket

--- public/scripts/appLogManage.js
+++ public/scripts/appLogManage.js
@@ -29,6 +29,7 @@ function getOutput(action){
 		res = res.replace(/'/g, '&acute;');
 		res = res.replace(/"/g, '&quot;');
 		res = res.replace(/\|/g, '&brvbar;');
+		alert(res);
 		$("#"+action).html(res);
 		window.setTimeout(function(){	
 			getOutput(action);
