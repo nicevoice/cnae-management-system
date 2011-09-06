a58f66f0aa4b6066fb098e8d670a6d7e4f21ed7f
dead-horse <dead_horse@qq.com>
2011-09-06 17:24:54 +0800

    remove something

--- public/scripts/coopManage.js
+++ public/scripts/coopManage.js
@@ -58,7 +58,7 @@ function submitInvite(){
         html += '>参与者</option><option value="3"';
         if(data.role==3) html += " selected";
         html+='>观察者</option></select></td></tr>';
-      	$(html).insertAfter("tr:last");
+      	$("#allCoops").html($("#allCoops").html()+=html);
       	$("tr:last a").click(deleteCoop);
       	}
    	}
