0fd9a24d74e0e843eba3b5bec212686f382b2e5a
dead-horse <dead_horse@qq.com>
2011-09-06 16:17:27 +0800

    remove something

--- init/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req, res){
-      res.writeHead(200, {"content-type":"text/html"});
-      res.end("hello,cnode app engine!");
-      }).listen(80);
-console.log("server started at port 8081");
