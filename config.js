432eb45c98e16940742bc0c4fe6dd1320bd58a1b
dead-horse <dead_horse@qq.com>
2011-09-13 02:05:58 +0800

    add logs

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ var pathutil = require('path');
 var mongo = require("mongoskin");
 var fs = require('fs');
 var configInfo = eval('('+fs.readFileSync("./config.json")+')');
+
+//是否debug模式
 var debug = exports.debug = configInfo.debug;
 
 var root_dir = pathutil.dirname(__dirname)
@@ -11,74 +13,65 @@ var root_dir = pathutil.dirname(__dirname)
 exports.proxy_sock = pathutil.join(cnae_dir, 'run', 'proxy.sock');
 exports.listen_sock_dir = pathutil.dirname(exports.proxy_sock);
 
-exports.session_secret = "sakjfewf_cnaemngm";
-var port = exports.port = 2012;
-exports.email = "heyiyu.deadhore@gmail.com";
-exports.site_name = "Node App Engine";
-exports.site_desc = '';
+exports.session_secret = configInfo.session_secret;
+var port = exports.port = configInfo.port;
+exports.email = configInfo.email;
+exports.site_name = configInfo.site_name;
 
 exports.db_url = configInfo.dbUserName+":"+configInfo.dbPassword+"@127.0.0.1:27017/"+configInfo.dbName;
 exports.db = mongo.db(this.db_url);
 /***
  * 用户信息：email,nickName,password,realName,telNumber,mainPage
  * */
-exports.db_user = "nae_user";
+exports.db_user = configInfo.collections.user;
 /***
  * 应用成员信息:appDomain, appName, email, role
  * 总共4种身份，从0～3：创建者，管理者，参与者，观察者
  */
-exports.db_app_mem = "nae_app_members";
+exports.db_app_mem = configInfo.collections.app_member;
 /***
  * 应用基本信息：appDomain, appName（有冗余，可以减少多表查询）,appDescribe,appState,appCreateTime...//todo
  */
-exports.db_app_basic ="nae_app_basic_infos";
-exports.db_app_records = "nae_app_manage_records";
-exports.db_inviteCode = "nae_inviteCode";
-exports.db_app_todo = "nae_app_todos";
+exports.db_app_basic =configInfo.collections.app_basic;
+exports.db_app_records = configInfo.collections.app_record;
+exports.db_inviteCode = configInfo.collections.inviteCode;
+exports.db_app_todo = configInfo.collections.app_todo;
 //session
-exports.session_timeOut = 3600000*24*14;
+exports.session_timeOut = configInfo.timeOut;
 //正则
-exports.regEmail = /^[a-zA-Z0-9_\.\-\+]+@(\w+)\.[\w\.]{2,8}$/;
-exports.regPass = /^(\w){6,20}$/;
-exports.regName = /^([a-zA-Z0-9._\-]){2,20}$/;
+exports.regEmail = configInfo.regEmail;
+exports.regPass = configInfo.regPass;
+exports.regName = configInfo.regName;
 //log
 var log = require("./lib/log");
-exports.logWithFile = log.create(log.INFO, './logs/system.log');
+exports.logWithFile = log.create(log.INFO, configInfo.logPath);
 //mail的配置
 //协作邀请邮件的模板
-exports.mailTitle =  'Node App Engine协作邀请函';
-exports.mailContent = fs.readFileSync("./mailTemplate/coopInviteMail.html", "utf-8");
+exports.mailTitle =  configInfo.mailTitle;
+exports.mailContent = fs.readFileSync(configInfo.mailContentPath, "utf-8");
 //发送邀请函邮件的模板
-exports.inviteMailTitle = "Node App Engine邀请码";
-exports.inviteMailContent = fs.readFileSync("./mailTemplate/inviteCodeMail.html", "utf-8");
-exports.admin = "dead_horse@qq.com";
+exports.inviteMailTitle = configInfo.inviteMailTitle;
+exports.inviteMailContent = fs.readFileSync(configInfo.inviteMailContentPath, "utf-8");
+exports.admin = configInfo.admin;
 exports.smtp = {
-    host: 'smtp.gmail.com',
-    port: 587,
-    ssl: false,
-    use_authentication: true,
-    user: "heyiyu.deadhorse@gmail.com",
-    pass: "heyiyuaaqq12"
+    host: configInfo.smtp.host,
+    port: configInfo.smtp.port,
+    ssl: configInfo.smtp.ssl,
+    use_authentication: configInfo.smtp.use_authentication,
+    user: configInfo.users,
+    pass: configInfo.pass
 };
 
-exports.uploadDir = pathutil.join(cnae_dir, 'apps');
+exports.uploadDir = configInfo.uploadDir;
 exports.tempDir = configInfo.tempDir;
-exports.socketPort = 1128;
+exports.socketPort = configInfo.socketPort;
 
 //管理员帐号表
-exports.admins = ["dead_horse@qq.com","fengmk2@gmail.com",
-				  "kunfirst@gmail.com", "q3boy1@gmail.com"];
+exports.admins = configInfo.admins;
 
 //邀请码注册链接
-exports.inviteHref = "http://cnodejs.net:"+port+"/regist?code=";
-//操作应用上下线的请求参数 todo
-exports.options = {
-    host: '127.0.0.1',
-    port: 1127,
-    method: 'post'
-};	
-
+exports.inviteHref = configInfo.inviteHref;
 if(!debug) {
     exports.port = 80;
-    exports.inviteHref = "http://cnodejs.net/regist?code=";
+    exports.inviteHref = configInfo.debugInviteHref;
 }
