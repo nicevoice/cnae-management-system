548bc03c71f2236d19965c40fdb817ae3bd5ea5d
dead-horse <dead_horse@qq.com>
2011-09-14 01:15:16 +0800

    todos

--- controllers/appDevelop.js
+++ controllers/appDevelop.js
@@ -464,7 +464,7 @@ exports.showTodo = function(req, res){
     }
     else {
       var userEmails = [], uhash={};
-      for(var i=0, len=data.length; i<length; ++i){
+      for(var i=0, len=data.length; i<len; ++i){
         if (uhash[data[i].email]) {
           uhash[data[i].email] = true;
           userEmails.push(data[i].email);
