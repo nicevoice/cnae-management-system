65596bcdbda0acc7465d4a5297bd1ec9c729b618
dead-horse <dead_horse@qq.com>
2011-09-15 15:32:22 +0800

    add Retrive

--- public/scripts/appSquare.js
+++ public/scripts/appSquare.js
@@ -46,14 +46,11 @@ function renderApp(app){
   if(app.port && app.port!=80){
     port=":"+app.port;
   }
-  if(app.appDes===""){
-    appDes = " ";
-  }
   var div = '<div class="app-info">' +
-            '<p><img src="/images/arrow.gif"></img><span class="appTitle">'+
+            '<p><img src="/images/arrow.gif"></img><span class="app-title">'+
             app.appName+'</span>&nbsp;&nbsp;<a href="http://'+app.appDomain+'.cnodejs.net'+port+'" target="_blank">' +
             app.appDomain+'.cnodejs.net</a></p>' + 
-            '<p class="app-des">'+app.appDes+'</p>' +
+            '<p class="app-des">描述：'+app.appDes+'</p>' +
             '<p class="app-mem">'+app.creatorNickName + '创建于' + app.appCreateDate +' | 有'+app.memberNums +'个参与者</p></div>';
   return div;
 }
