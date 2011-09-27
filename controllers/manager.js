f4ec5b7b5ce3b95e1c4e667074783dbee3cd9232
dead-horse <dead_horse@qq.com>
2011-09-27 14:39:26 +0800

    修改log顺序

--- controllers/manager.js
+++ controllers/manager.js
@@ -415,7 +415,7 @@ exports.getStdOutput = function(req, res){
     var lines = data.split('\n');
     if (lines&&lines.length>0) {
       lines.reverse();
-      console.log(lines.shift()+lines.length);
+      console.log(lines[0]+lines.length);
       while(lines.length>0 && lines[0].indexOf("/home/admin/cnae/git/cnode-app-engine/lib/modules/net.js") !== -1){
         console.log(lines.shift());
       } 
