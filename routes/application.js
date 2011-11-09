f6130575566005a69216aa181479d0ea048b4417
q3boy <q3boy1@gmail.com>
2011-11-09 16:35:37 +0800

    修正router

--- routes/application.js
+++ routes/application.js
@@ -8,6 +8,7 @@ module.exports = function(app){
   app.get("/", hasLogin, ctrApplication.show);
   app.get("/application", hasLogin, ctrApplication.show);
   app.get("/application/newApp", hasLogin, ctrApplication.showNewApp);
+  app.post("/getOwnAuth", hasLogin, ctrApplication.getOwnAuthInfo);
   app.post("/createApp", hasLogin, ctrApplication.createApp);
   app.post("/delete", hasLogin, ctrApplication.deleteApp);
   app.post("/join", hasLogin, ctrApplication.joinApp);
