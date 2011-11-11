d97c7f6e6032db034130b760ffa950b49cc5927b
dead-horse <dead_horse@qq.com>
2011-11-11 14:36:42 +0800

    rebuild

--- test/lib/httpReq.js
+++ test/lib/httpReq.js
@@ -1,4 +1,7 @@
-var http = require('http'), url = require('url'), httpReq = require('../../src/lib/httpReq').httpReq, testCase = require('nodeunit').testCase;
+var http = require('http'),
+ url = require('url'), 
+ httpReq = require('../../src/lib/httpReq').httpReq, 
+ testCase = require('nodeunit').testCase;
 
 var optionsQuery = {
   "host" : "127.0.0.1",
@@ -13,75 +16,78 @@ var optionsNoQuery = {
 var optionsReturnNotJson = {
   "host" : "127.0.0.1",
   "path" : "/",
-  "port" : 2014  
+  "port" : 2014
 }
 var optionsNotConnect = {
   "host" : "127.0.0.1",
   "path" : "/",
-  "port" : 2015  
+  "port" : 2015
 }
-var serverOpen = false;
-      http.createServer(function(req, res) {
-        var path = url.parse(req.url).pathname;
-        console.log(path);
-        if(path === "/unit_test") {
-          var test = url.parse(req.url, true).query.test;
-          if(test){
-          body = new Buffer(JSON.stringify({
-            status : "ok",
-            query : test
-          }));
-          }else{
-          body = new Buffer(JSON.stringify({
-            status : "error",
-            msg : "no query test"
-          }));            
-          }
-          res.writeHead(200, {
-            "Content/type" : "text/json",
-            "Content/length" : body.length
-          });
-          res.end(body);
-        } else {
-          res.writeHead(404, {
-            "Content/type" : "text/html"
-          });
-          res.end("404 not found");
-        }
-      }).listen(2014);
+http.createServer(function(req, res) {
+  var path = url.parse(req.url).pathname;
+  console.log(path);
+  if(path === "/unit_test") {
+    var test = url.parse(req.url, true).query.test;
+    if(test) {
+      body = new Buffer(JSON.stringify({
+        status : "ok",
+        query : test
+      }));
+    } else {
+      body = new Buffer(JSON.stringify({
+        status : "error",
+        msg : "no query test"
+      }));
+    }
+    res.writeHead(200, {
+      "Content/type" : "text/json",
+      "Content/length" : body.length
+    });
+    res.end(body);
+  } else {
+    res.writeHead(404, {
+      "Content/type" : "text/html"
+    });
+    res.end("404 not found");
+  }
+}).listen(2014);
 module.exports = testCase({
   setUp : function(callback) {
-      //callback();
-      setTimeout(function(){callback()}, 10);
+    //callback();
+    setTimeout(function() {callback()
+    }, 10);
   },
   tearDown : function(callback) {
     callback();
   },
   test_good_path_with_query : function(test) {
-      httpReq(optionsQuery, function(data) {
-        test.deepEqual(data, {status:"ok", query:"unitTest"}, "http req with query error");
-        test.done();
+    httpReq(optionsQuery, function(data) {
+      test.deepEqual(data, {
+        status : "ok",
+        query : "unitTest"
+      }, "http req with query error");
+      test.done();
     });
   },
-
-  test_good_path_without_query : function(test){
-    httpReq(optionsNoQuery, function(data){
-        test.deepEqual(data, {status:"error", msg:"no query test"}, "http req without query error");
-        test.done();      
+  test_good_path_without_query : function(test) {
+    httpReq(optionsNoQuery, function(data) {
+      test.deepEqual(data, {
+        status : "error",
+        msg : "no query test"
+      }, "http req without query error");
+      test.done();
     })
   },
-  
-    test_http_return_not_json : function(test){
-    httpReq(optionsReturnNotJson, function(data){
-        test.deepEqual(data, "http returns not json", "when http return not json error");
-        test.done();      
+  test_http_return_not_json : function(test) {
+    httpReq(optionsReturnNotJson, function(data) {
+      test.deepEqual(data, "http returns not json", "when http return not json error");
+      test.done();
     })
   },
-  
-  test_http_not_connect : function(test){
-    httpReq(optionsNotConnect, function(data){
-        test.deepEqual(data, "ECONNREFUSED, Connection refused", "when http return not json error");
-        test.done();      
-    })    
+  test_http_not_connect : function(test) {
+    httpReq(optionsNotConnect, function(data) {
+      test.deepEqual(data, "ECONNREFUSED, Connection refused", "when http return not json error");
+      test.done();
+    })
   }
 })
--- test/lib/socket.js
+++ test/lib/socket.js
@@ -3,18 +3,37 @@ var testCase = require('nodeunit').testCase,
     onOff = socket.onOff,
     getLog = socket.getLog;
 module.exports = testCase({
-  setUp : function(){
+  setUp : function(callback){
     callback();
   },
-  tearDown : function(){
+  tearDown : function(callback){
     callback();
   },
-  test_on_ok : function(test){
+  test_connection_start_exsit : function(test){
     var action = "start",
-        app = "testUnit";
+        app = "unittest";
         onOff(action, app, function(data){
-          console.log(data);
+          test.deepEqual({status:data.status, code:data.code},
+            {status:"ok", code:101}, "start exsist error");
           test.done();
         })
   },
+  test_connection_start_not_exsit:function(test){
+    var action = "start",
+        app = "unittest1";
+        onOff(action, app, function(data){
+          test.deepEqual({status:data.status, code:data.code},
+            {status:"error", code:101}, "start not exsist error");
+          test.done();
+        });    
+  },
+  test_connection_start_running:function(test){
+    var action = "start",
+        app = "unittest";
+        onOff(action, app, function(data){
+          test.deepEqual({status:data.status, code:data.code},
+            {status:"error", code:101}, "start not exsist error");
+          test.done();
+        });        
+  }
 })
