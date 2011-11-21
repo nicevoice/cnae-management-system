234555f908a4f1d5462372e571d9ec77a2b9b918
q3boy <q3boy1@gmail.com>
2011-11-21 16:23:41 +0800

    删除给所有用户添加邀请码

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -14,7 +14,7 @@ user = config.dbInfo.collections.user;
  * @param {} res
  */
 exports.show = function(req, res){
-    find(user, {}, function(err, data){
+/*    find(user, {}, function(err, data){
         for(var i=0, len=data.length; i!=len; ++i){
             var codes = [];
             for(var j=0; j!=config.maxCode; ++j){
@@ -26,7 +26,7 @@ exports.show = function(req, res){
                 update(user, {email:email}, {$set:{inviteCode:codes}});
             })(codes, data[i].email);
         }
-    })
+    })*/
     res.redirect('/userCenter/userInfo');
 };
 /***
