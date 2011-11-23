f4d438fbd28f4205c4d7ae3f6054bc643513438b
dead horse <dead_horse@qq.com>
2011-11-23 10:58:09 +0800

    添加系统监控

---
+++ src/routes/monitor.js
@@ -0,0 +1,13 @@
+var middleware = require('./middleware'),
+    hasLogin = middleware.hasLogin,
+    isAdmin = middleware.isAdmin,
+    labs = require('../config').labs,
+    ctrMonitor = require('../controllers/monitor');
+    
+module.exports = function(app){
+  //邀请码模块
+  if (!labs) {
+      app.get("/monitor", hasLogin, isAdmin, ctrMonitor.show);
+      app.get("/load_monitor", hasLogin, inAdmin, ctrMonitor.load);
+  }
+}
