86321142128825d7545c8249d1f050ff921f1f83
heyiyu.pt_52624 <heyiyu.pt@edp2.sqa.cm4>
2011-11-30 17:11:11 +0800

    修改路径

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 var pathutil = require('path');
 var mongo = require("mongoskin");
 var fs = require('fs');
-var configInfo = JSON.parse(fs.readFileSync('./config.json').toString());
+var configInfo = JSON.parse(fs.readFileSync(__dirname+'/config.json').toString());
 var log = require("./lib/log");
 //是否debug模式
 var debug = configInfo.switchs.debug;
@@ -24,12 +24,12 @@ configInfo.logWithFile = log.create(log.INFO, configInfo.logPath);
 
 //读取mail正文
 var mail = configInfo.mail;
-mail.coopMailContent = fs.readFileSync(mail.coopMailContentPath, "utf-8");//合作邀请
-mail.inviteMailContent = fs.readFileSync(mail.inviteMailContentPath, "utf-8");//邀请码
-mail.retrieveMailContent = fs.readFileSync(mail.retrieveMailContentPath, "utf-8");//找回密码
-mail.applyMailContent = fs.readFileSync(mail.applyMailContentPath, "utf-8");//申请项目
-mail.agreeMailContent = fs.readFileSync(mail.agreeMailContentPath, "utf-8");//同意
-mail.refuseMailContent = fs.readFileSync(mail.refuseMailContentPath, "utf-8");//拒绝
+mail.coopMailContent = fs.readFileSync(__dirname+mail.coopMailContentPath, "utf-8");//合作邀请
+mail.inviteMailContent = fs.readFileSync(__dirname+mail.inviteMailContentPath, "utf-8");//邀请码
+mail.retrieveMailContent = fs.readFileSync(__dirname+mail.retrieveMailContentPath, "utf-8");//找回密码
+mail.applyMailContent = fs.readFileSync(__dirname+mail.applyMailContentPath, "utf-8");//申请项目
+mail.agreeMailContent = fs.readFileSync(__dirname+mail.agreeMailContentPath, "utf-8");//同意
+mail.refuseMailContent = fs.readFileSync(__dirname+mail.refuseMailContentPath, "utf-8");//拒绝
 
 configInfo.inviteHref = "http://cnodejs.net:"+port+"/regist?code=";
 if(!debug) {
@@ -37,6 +37,9 @@ if(!debug) {
     configInfo.inviteHref = "http://cnodejs.net/regist?code=";
 }
 
+//labs
+configInfo.labs= configInfo.switchs.labs;
+console.log(exports.labs);
 if(configInfo.switchs.labs){
   if(configInfo.switchs.daily){
     configInfo.toplevelDomain = configInfo.toplevelDomainDaily;
