fbf81d1760a7175cca718d67f54cf22a154f6da9
heyiyu.pt_52624 <heyiyu.pt@edp2.sqa.cm4>
2011-12-13 11:11:03 +0800

    fix xss

--- src/public/scripts/appManageTodo.js
+++ src/public/scripts/appManageTodo.js
@@ -29,7 +29,7 @@ var tplTodos = '<div class="post subContent">'+
                '<p> <input type="text" id="titleContent" name="title" class="longInput" /></p>'+
                '</form></div>'+
                '<div class="todos"><ul>$todos$</ul></div>',
-     tplFinished = '<li class="finished">'+
+     tplFinished = '<span style="width:400px"><li class="finished">'+
                '<img src="/images/ok.gif"></img> $nickName$ : <del>$title$</del><span class="todoAction">'+
                '<a href="javascript:void(0)" id="$email$#$title$" class="doRecover">恢复</a>'+
                ' , <a href="javascript:void(0)" class="doDelete">删除</a></span></li>';
