591d636ba01518129accb5be71892b1b2ebe5642
heyiyu.pt_52624 <heyiyu.pt@edp2.sqa.cm4>
2011-12-13 11:19:57 +0800

    fix css

--- src/public/scripts/appManageTodo.js
+++ src/public/scripts/appManageTodo.js
@@ -29,14 +29,14 @@ var tplTodos = '<div class="post subContent">'+
                '<p> <input type="text" id="titleContent" name="title" class="longInput" /></p>'+
                '</form></div>'+
                '<div class="todos"><ul>$todos$</ul></div>',
-     tplFinished = '<span style="width:400px"><li class="finished">'+
+     tplFinished = '<li class="finished">'+
                '<img src="/images/ok.gif"></img> $nickName$ : <del>$title$</del><span class="todoAction">'+
                '<a href="javascript:void(0)" id="$email$#$title$" class="doRecover">恢复</a>'+
-               ' , <a href="javascript:void(0)" class="doDelete">删除</a></span></li></span>';
-     tplNotFinished = '<span style="width:400px"><li>'+
+               ' , <a href="javascript:void(0)" class="doDelete">删除</a></span></li>';
+     tplNotFinished = '<li>'+
                '$nickName$ : $title$ <span class="todoAction">'+
                '<a href="javascript:void(0)" id="$email$#$title$" class="doFinish">完成</a>'+
-               ' , <a href="javascript:void(0)" class="doDelete">删除</a></span></li></span>';
+               ' , <a href="javascript:void(0)" class="doDelete">删除</a></span></li>';
 function renderTodoContent(todos){
     var todoContent = "";
     for(var i=0, len=todos.length; i!=len; ++i){
