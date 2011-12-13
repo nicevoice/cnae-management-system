6b81eb7c76a5664479a2d75a7e99ab8792ec22c9
heyiyu.pt_52624 <heyiyu.pt@edp2.sqa.cm4>
2011-12-13 16:33:24 +0800

    html special

--- src/server.js
+++ src/server.js
@@ -6,9 +6,9 @@ var config = require('./config');
 var path = require('path');
 if(cluster.isMaster){
   fs.writeFileSync(path.dirname(config.logPath)+'/worker.num', '1');
-  if(config.switchs.debug&&0){
+  if(config.switchs.debug){
     app.listen(config.port);
-    console.log("server start listen on "+config.port+' by '+process.pid);        
+    console.log("server start listen on "+config.port+' by '+process.pid);       
   }else{
     var num = Math.ceil(require('os').cpus().length/2);//cpu的一半
     var workers = {}, started_success = 0;
@@ -37,46 +37,47 @@ if(cluster.isMaster){
       }
     };
     setTimeout(initWorker, 200);
-  }
-  cluster.on('death', function(worker){
-    console.log('worker ' + worker.pid + 'died');
-    delete workers[worker.pid];
-    workers[worker.pid] = cluster.fork();
-  })
-  var pid_path = __dirname + '/server.pid';
-  fs.writeFile(pid_path, '' + process.pid);
-  process.on('exit', function () {
-  //    console.log('Got SIGINT.  Press Control-D to exit.', arguments);
-      try {
-          fs.unlinkSync(pid_path);
-      } catch (e){
+
+    cluster.on('death', function(worker){
+      console.log('worker ' + worker.pid + 'died');
+      delete workers[worker.pid];
+      workers[worker.pid] = cluster.fork();
+    })
+    var pid_path = __dirname + '/server.pid';
+    fs.writeFile(pid_path, '' + process.pid);
+    process.on('exit', function () {
+    //    console.log('Got SIGINT.  Press Control-D to exit.', arguments);
+        try {
+            fs.unlinkSync(pid_path);
+        } catch (e){
+        }
+        for(var k in workers){
+          workers[k].kill();
+        }
+        process.exit();
+    });
+      // process admin server
+    http.createServer(function(req, res) { //开启一个监听本地的服务，通过HTTP控制restart和stop
+      if (req.url === '/restart') {
+        for (var k in workers) {
+          workers[k].kill();
+        }
+      } else if (req.url === '/stop') {
+        for (var k in workers) {
+          workers[k].kill();
+        }
+        process.nextTick(function() {
+          process.kill();
+        });
+      } else if (req.url === '/status') {
+        for (var k in workers) {
+          res.write('worker ' + k + ' alive\n');
+        }
       }
-      for(var k in workers){
-        workers[k].kill();
-      }
-      process.exit();
-  });
-    // process admin server
-  http.createServer(function(req, res) { //开启一个监听本地的服务，通过HTTP控制restart和stop
-    if (req.url === '/restart') {
-      for (var k in workers) {
-        workers[k].kill();
-      }
-    } else if (req.url === '/stop') {
-      for (var k in workers) {
-        workers[k].kill();
-      }
-      process.nextTick(function() {
-        process.kill();
-      });
-    } else if (req.url === '/status') {
-      for (var k in workers) {
-        res.write('worker ' + k + ' alive\n');
-      }
+      res.end(req.url + '\n');
+    }).__listen(config.adminPort, '127.0.0.1');
+    console.log('admin server listen on 2014');
     }
-    res.end(req.url + '\n');
-  }).__listen(config.adminPort, '127.0.0.1');
-  console.log('admin server listen on 2014');
 }else{
   app.listen(config.port, function(){
     process.send({cmd:'started', pid:process.pid});
