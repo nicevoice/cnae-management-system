533224f5d3d8118154b7e20af8a4739affd3f1a7
heyiyu.pt_52624 <heyiyu.pt@edp2.sqa.cm4>
2012-01-10 17:23:25 +0800

    fix to zsh

--- src/shells/mongoAllocator.sh
+++ src/shells/mongoAllocator.sh
@@ -1,2 +1,2 @@
-#! /bin/sh
+#! /bin/zsh
 echo "use admin\n db.auth('$5','$6')\n use $1 \n db.addUser('$2', '$3')" | mongo -port=$4
--- src/shells/mongoDeletor.sh
+++ src/shells/mongoDeletor.sh
@@ -1,2 +1,2 @@
-#! /bin/sh
+#! /bin/zsh
 echo "use admin\n db.auth('$3','$4')\n use $1 \n db.dropDatabase()" | mongo -port=$2
